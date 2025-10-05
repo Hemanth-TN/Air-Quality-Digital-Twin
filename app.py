95748d415afecc03d2aef768f2d361ded7502aba
Hemanth-TN <hemanth.tnagaraju@gmail.com>
2025-10-04 19:52:28 -0500

    updated port of app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ app.layout = dbc.Container([
 
 server = app.server
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=True)
+    app.run_server(host="0.0.0.0", port=8080, debug=False)
